3ce97dfc0f87f4c02b40656384e78065f266165b
zizo <zizo@zizo-HP-Compaq-8200-Elite-CMT-PC>
2017-08-26 20:34:26 +0200

    update elastic search

--- search.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('AMP-Access-Control-Allow-Source-Origin: http://localhost');
-$name=$_POST['booksearch'];
-
-$response = array('title' => 'sssssss','description'=>'dddddddddd');
-echo json_encode($response);
